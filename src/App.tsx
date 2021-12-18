9ec20fc97b7642e13582428766de51066d88bdb6
khadetou <khadetou96@gmail.com>
2021-12-18 22:45:09 +0000

    basic code skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import './App.css';
 
 function App() {
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
+  const onClick = () => {
+    setCode(input);
+  };
   return (
     <div>
-      <h1>Hello World</h1>
+      <textarea value={input} onChange={(e) => setInput(e.target.value)}></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
     </div>
   );
 }
