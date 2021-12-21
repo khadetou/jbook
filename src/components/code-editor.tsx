c9b83f8eee62f6a60eeb4041ec9ae632cc3c7568
khadetou <khadetou96@gmail.com>
2021-12-21 16:33:08 +0000

    a few more options

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -5,6 +5,19 @@ const CodeEditor = () => {
         height="90vh"
         language="javascript"
         theme="vs-dark"
+        options={
+            {
+                wordWrap: 'on',
+                minimap: { enabled: false },
+                showUnused: false,
+                folding: false,
+                lineNumbersMinChars: 3,
+                fontSize: 16,
+                mouseWheelZoom: true,
+                scrollBeyondLastLine: false,
+                automaticLayout: true
+            }
+        }
     />
 }
 
