23c1f99b98a8629196f341ac38aa89c5c92f7e79
khadetou <khadetou96@gmail.com>
2021-12-21 18:44:53 +0000

    adding prettier to our editor

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,5 +1,6 @@
 import MonacoEditor, { OnChange, OnMount } from '@monaco-editor/react';
-
+import prettier from 'prettier';
+import parser from 'prettier/parser-babel';
 
 interface CodeEditorProps {
     initialValue: string;
@@ -15,28 +16,35 @@ const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue, onChange }) => {
         editor.getModel()!.updateOptions({ tabSize: 2 });
     }
 
-    return <MonacoEditor
-        onChange={onChangeHandler}
-        onMount={onMountHandler}
-        value={initialValue}
-        height="90vh"
-        language="javascript"
-        theme="vs-dark"
-        options={
-            {
-                wordWrap: 'on',
-                minimap: { enabled: false },
-                showUnused: false,
-                folding: false,
-                lineNumbersMinChars: 3,
-                fontSize: 16,
-                mouseWheelZoom: true,
-                scrollBeyondLastLine: false,
-                automaticLayout: true
-            }
-        }
+    const onFormatClick = () => {
 
-    />
+    }
+    return (
+        <div>
+            <button>Format</button>
+            <MonacoEditor
+                onChange={onChangeHandler}
+                onMount={onMountHandler}
+                value={initialValue}
+                height="90vh"
+                language="javascript"
+                theme="vs-dark"
+                options={
+                    {
+                        wordWrap: 'on',
+                        minimap: { enabled: false },
+                        showUnused: false,
+                        folding: false,
+                        lineNumbersMinChars: 3,
+                        fontSize: 16,
+                        mouseWheelZoom: true,
+                        scrollBeyondLastLine: false,
+                        automaticLayout: true
+                    }
+                }
+
+            />
+        </div>)
 }
 
 export default CodeEditor;
