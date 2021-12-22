bfe01d3075108c7e2df48294f5f5efafe7faa3c2
khadetou <khadetou96@gmail.com>
2021-12-22 20:24:46 +0000

    extracting bundling logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,24 @@
-import { useState, useEffect, useRef } from 'react';
-import * as esbuild from 'esbuild-wasm';
-import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
-import { fetchPlugin } from './plugins/fetch-plugin';
+import { useState } from 'react';
+
+
 import CodeEditor from './components/code-editor';
 import "bulmaswatch/superhero/bulmaswatch.min.css";
 import Preview from './components/preview';
+import bundle from './bundler';
 
 function App() {
-
-  //USE REF
-  const ref = useRef<any>();
-
 
   //USE STATE
   const [input, setInput] = useState<string | undefined>('');
   const [code, setCode] = useState('');
 
-  //ESBUILD 
-  const startService = async () => {
-    ref.current = await esbuild.startService({
-      worker: true,
-      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
-    });
-  }
 
   //FUNCTION TO EXECUTE CODE
   const onClick = async () => {
-    if (!ref.current) {
-      return;
-    }
-
-
-
-    const result = await ref.current.build({
-      entryPoints: ['index.js'],
-      bundle: true,
-      write: false,
-      plugins: [
-        unpkgPathPlugin(),
-        fetchPlugin(input)
-      ],
-      define: {
-        'process.env.NODE_ENV': '"development"',
-        global: 'window',
-      }
-    });
-
-    setCode(result.outputFiles[0].text);
+    const output = await bundle(input);
+    setCode(output);
   };
 
-  //USE EFFECT
-  useEffect(() => {
-    startService();
-  }, []);
 
 
   //RENDER
