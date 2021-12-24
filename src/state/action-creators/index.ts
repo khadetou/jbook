e9cbe55c4496ccb3bc3ea081a1b6a9d25963ee40
khadetou <khadetou96@gmail.com>
2021-12-24 15:05:08 +0000

    Action creator implementation

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -1,10 +1,47 @@
+import { CellTypes } from "./../cell";
+import { ActionTypes } from "../action-types";
 import {
   UpdateSellAction,
   MoveSellAction,
   DeleteSellAction,
   InsertSellBeforeAction,
 } from "./../actions/index";
-export const updateCell = (): UpdateSellAction => ({});
-export const moveCell = (): MoveSellAction => ({});
-export const deleteCell = (): DeleteSellAction => ({});
-export const insertCellBefore = (): InsertSellBeforeAction => ({});
+export const updateCell = (id: string, content: string): UpdateSellAction => {
+  return {
+    type: ActionTypes.UPDATE_SELL,
+    payload: {
+      id,
+      content,
+    },
+  };
+};
+export const moveCell = (
+  id: string,
+  direction: "up" | "down"
+): MoveSellAction => {
+  return {
+    type: ActionTypes.MOVE_SELL,
+    payload: {
+      id,
+      direction,
+    },
+  };
+};
+export const deleteCell = (id: string): DeleteSellAction => {
+  return {
+    type: ActionTypes.DELETE_SELL,
+    payload: id,
+  };
+};
+export const insertCellBefore = (
+  id: string,
+  cellType: CellTypes
+): InsertSellBeforeAction => {
+  return {
+    type: ActionTypes.INSERT_SELL_BEFORE,
+    payload: {
+      id,
+      type: cellType,
+    },
+  };
+};
