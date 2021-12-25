63381c55074e58882db9bb81c9d992f779cf81ad
khadetou <khadetou96@gmail.com>
2021-12-25 18:38:32 +0000

    style fixing

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -22,7 +22,8 @@ const CellList: React.FC = () => {
     return (
         <div>
             {renderedCells}
-            <AddCell nextCellId={null} />
+            <AddCell forceVisible={cells.length === 0} nextCellId={null} />
+
         </div>
     )
 }
